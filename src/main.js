70fef2d7a57656308a436ea4a896380d526089a3
Etefa Guyassa Dinssa <etefag@gmail.com>
2023-11-19 16:28:26 +0100

    Add calculator project

--- src/main.js
+++ src/main.js
@@ -2,18 +2,18 @@ let display = document.querySelector('.display');
 let buttons = document.querySelector('.buttons');
 
 buttons.addEventListener('click', function (event) {
-    if (event.target.tagName === 'BUTTON') {
+    if (event.target.tagName == 'BUTTON') {
         let buttonValue = event.target.textContent;
 
-        if (buttonValue === '=') {
+        if (buttonValue == '=') {
             try {
                 display.value = eval(display.value).toString();
             } catch (error) {
                 display.value = 'Error';
             }
-        } else if (buttonValue === 'AC') {
+        } else if (buttonValue == 'AC') {
             display.value = '';
-        } else if (buttonValue === 'DEL') {
+        } else if (buttonValue == 'DEL') {
             display.value = display.value.slice(0, -1);
         } else {
             display.value += buttonValue;
